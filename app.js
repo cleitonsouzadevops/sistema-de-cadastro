1023e856af9a759b520e502f97fb8680614c00a2
cleitonsouzadevops <cleitonsouzadevops@gmail.com>
2023-02-06 15:36:59 -0300

    estou restaurando os arquivos para a versão anterior

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-
+console.log ("Rodando o sistema de gerenciamento de jogos")
+console.log ("Aplicação está se coenctadno ao banco de dados")
+console.log ("O Cleiton fez esse commit sem você ver")
+console.log ("Enviando para o GitHub")
