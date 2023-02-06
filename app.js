fa837f46bf4cb0001fb7e4997a5588a6b5aa7211
cleitonsouzadevops <cleitonsouzadevops@gmail.com>
2023-02-06 15:13:07 -0300

    alterando app js

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log ("Aplicação está se coenctadno ao banco de dados")
-console.log ("O Cleiton fez esse commit sem você ver")
-console.log ("Enviando para o GitHub")
