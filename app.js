c340233ef1c5720009f962d69dc011441663275e
cleitonsouzadevops <cleitonsouzadevops@gmail.com>
2023-02-06 14:56:34 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
 console.log ("Aplicação está se coenctadno ao banco de dados")
 console.log ("O Cleiton fez esse commit sem você ver")
+console.log ("AEnviando para o GitHub")
